c88bdf1dc415ae6d297c9a6f114e0f7e0e1ad724
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2015-02-11 22:52:21 -0800

    Add individual clicks for multi cats

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 var cats = [0,0,0,0,0];
 
 for (var i = 0; i < cats.length; i++){
-    $('#cats').append('<p class="catlist" id="' + i + '">Cat Number ' + i + '</div>')
-    $('#' + i).on('click', (function(im){
 
+    $('#cats').append('<p class="catlist" id="cat' + i + '">Cat Number ' + i + '</p>')
+    $('#catpic').append('<img class="nocat" id="pic' + i + '" src = "img/cat' + i + '.jpg" />')
+
+    // var img = document.createElement('img');
+
+    // img.addEventListener('click', (function(im){
+    //     return function() {
+    //         console.log(im);
+    //         cats[im] += 1;
+    //         $('#clicks').text(cats[im]);
+    //         console.log(im);
+    //     }
+    // })(i));
+
+    $('#cat' + i).on('click', (function(im){
         return function() {
-            $('img').attr("src", "img/cat" + im + '.jpg');
+            $('.cat').toggleClass('nocat').toggleClass('cat');
+            $('#pic' + im).toggleClass('nocat').toggleClass('cat');
+            $('#clicks').text(cats[im]);
+        }
+    })(i));
+
+    $('#pic' + i).on('click', (function(im){
+        return function() {
             cats[im] += 1;
-            console.log(cats[im]);
+            $('#clicks').text(cats[im]);
         }
     })(i));
 }
-
-$('.cat').click(function(e) {
-    clicks += 1;
-    $('#clicks').text(clicks);
-});
