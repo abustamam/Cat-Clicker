ec327cede795d042fd3a9aadbcebce8fd146ef7c
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2015-02-14 20:02:02 -0800

    Add MVO, and local storage

--- js/script.js
+++ js/script.js
@@ -1,33 +1,95 @@
-var cats = [0,0,0,0,0];
+$(function(){
 
-for (var i = 0; i < cats.length; i++){
+    var model = {
+        init: function(){
+            if (!localStorage.cats) {
+                localStorage.cats = JSON.stringify([
+                    {cat: 0, clicks: 0},
+                    {cat: 1, clicks: 0},
+                    {cat: 2, clicks: 0},
+                    {cat: 3, clicks: 0},
+                    {cat: 4, clicks: 0},
+                    {cat: 5, clicks: 0}
+                    ]);
+            }
+        },
 
-    $('#cats').append('<p class="catlist" id="cat' + i + '">Cat Number ' + i + '</p>')
-    $('#catpic').append('<img class="nocat" id="pic' + i + '" src = "img/cat' + i + '.jpg" />')
+        getAllCats: function() {
+            return JSON.parse(localStorage.cats);
+        },
 
-    // var img = document.createElement('img');
+        getClicks: function(catNum){
+            var data = JSON.parse(localStorage.cats);
+            return data[catNum]['clicks'];
+        },
 
-    // img.addEventListener('click', (function(im){
-    //     return function() {
-    //         console.log(im);
-    //         cats[im] += 1;
-    //         $('#clicks').text(cats[im]);
-    //         console.log(im);
-    //     }
-    // })(i));
-
-    $('#cat' + i).on('click', (function(im){
-        return function() {
-            $('.cat').toggleClass('nocat').toggleClass('cat');
-            $('#pic' + im).toggleClass('nocat').toggleClass('cat');
-            $('#clicks').text(cats[im]);
+        click: function(catNum) {
+            var data = JSON.parse(localStorage.cats);
+            data[catNum]['clicks'] += 1;
+            localStorage.cats = JSON.stringify(data);
+            return data[catNum]['clicks'];
         }
-    })(i));
+    };
 
-    $('#pic' + i).on('click', (function(im){
-        return function() {
-            cats[im] += 1;
-            $('#clicks').text(cats[im]);
+    var octopus = {
+        getCats: function() {
+            return model.getAllCats();
+        },
+
+        init: function() {
+            model.init();
+            catListView.init();
+            catImgView.init();
+        },
+
+        getClicks: function(catNum) {
+            return model.getClicks(catNum);
+        },
+
+        click: function(catNum) {
+            return model.click(catNum);
         }
-    })(i));
-}
+    };
+
+    var catListView = {
+        init: function() {
+            this.catList = $('#cats');
+            catListView.render();
+        },
+
+        render: function(){
+            var htmlStr = '';
+            this.catId = 0;
+            octopus.getCats().forEach(function(cat){
+                htmlStr += '<li class="cat" id="cat' + cat.cat + '">Cat Number ' + 
+                                cat.cat +
+                            '</li>';
+            });
+            this.catList.html( htmlStr );
+            $('.cat').each(function(catNum){
+                $('#cat' + catNum).on('click', (function(im){
+                    return function() {
+                        this.catId = im;
+                        catImgView.render(im);
+                    }
+                })(catNum));
+            });
+        }
+    };
+
+    var catImgView = {
+        init: function() {
+            this.catImg = $('#catpic');
+        },
+
+        render: function(num) {
+            this.catImg.html( '<img id="pic' + num + '" src="img/cat' + num + '.jpg" />' );
+            $('#pic' + num).on('click', function(){
+                $('#clicks').text(octopus.click(num));
+            });
+            $('#clicks').text(octopus.getClicks(num));
+        }
+    };
+
+    octopus.init();
+});
